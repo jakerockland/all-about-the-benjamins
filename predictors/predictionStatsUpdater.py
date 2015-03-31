030c11a1cd4787afdb3c7c5c588d29f3a89b3fea
kuba1 <kubastechly@gmail.com>
2015-03-30 21:48:35 -0700

    skeleton of predictionStatsUpdater done

---
+++ predictors/predictionStatsUpdater.py
@@ -0,0 +1,11 @@
+class PredictionStatsUpdater(object):
+
+	def PredictionStatsUpdater(self):
+		pass
+
+	def updatePredictionStats(self):
+		pass
+
+if __name__ == "__main__":
+	p = PredictionStatsUpdater()
+	p.updatePredictionStats()
