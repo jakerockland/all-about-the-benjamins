e2fdfdf508684ed35c71f2e46210b348b49b923d
Jake Rockland <jakerockland@email.arizona.edu>
2015-03-30 18:41:50 -0700

    made subreddit parameter

--- reddit.py
+++ reddit.py
@@ -16,26 +16,26 @@ def totimestamp(dt, epoch=datetime(1970,1,1)):
     td = dt - epoch
     return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6
 
-def postDeltas(type="new", num=100):
+def postDeltas(sub="all", type="new", num=100):
     r = praw.Reddit('Post volatility calculator 1.0 by u/benjamin-spanklin.'
                     'A simple Python scraper to calculate post volatility.')
 
     r.login('benjamin-spanklin','yhBAfBH38L7e')
 
-    all_subreddit = r.get_subreddit('all')
+    subreddit = r.get_subreddit(sub)
 
     if (type == "hot"):
-        posts = all_subreddit.get_hot(limit=num)
+        posts = subreddit.get_hot(limit=num)
     elif (type == "controversial"):
-        posts = all_subreddit.get_controversial(limit=num)
+        posts = subreddit.get_controversial(limit=num)
     elif (type == "rising"):
-        posts = all_subreddit.get_rising(limit=num)
+        posts = subreddit.get_rising(limit=num)
     elif (type == "top"):
-        posts = all_subreddit.get_top(limit=num)
+        posts = subreddit.get_top(limit=num)
     elif (type == "unmoderated"):
-        posts = all_subreddit.get_unmoderated(limit=num)
+        posts = subreddit.get_unmoderated(limit=num)
     else:
-        posts = all_subreddit.get_new(limit=num)
+        posts = subreddit.get_new(limit=num)
 
     now = float(math.trunc(totimestamp(datetime.utcnow())))
 
@@ -47,9 +47,9 @@ def postDeltas(type="new", num=100):
     return deltas
 
 def main():
-    hot_deltas = numpy.array(postDeltas("hot", 1000)) # change limit to +1000?
-    controversial_deltas = numpy.array(postDeltas("controversial", 1000)) # change limit to +1000?
-    rising_deltas = numpy.array(postDeltas("rising", 1000)) # change limit to +1000?
+    hot_deltas = numpy.array(postDeltas("all", "hot", 1000)) # change limit to +1000?
+    controversial_deltas = numpy.array(postDeltas("all", "controversial", 1000)) # change limit to +1000?
+    rising_deltas = numpy.array(postDeltas("all", "rising", 1000)) # change limit to +1000?
 
     print("Hot:\t" + str(numpy.mean(hot_deltas)) + "\t" + str(numpy.var(hot_deltas)))
     print("Controversial:\t" + str(numpy.mean(controversial_deltas)) + "\t" + str(numpy.var(controversial_deltas)))
