17193e64aa37157c858a639f263365d874c10a62
kuba1 <kubastechly@gmail.com>
2015-03-30 21:07:32 -0700

    wrote more of updater skeleton and some fxns

--- interpreter/updater.py
+++ interpreter/updater.py
@@ -1,10 +1,21 @@
+import json
 class Updater:
 	
 	def Updater(self):
 		pass
+	
+	def dumpToDataBase(predictions):
+		with open('predictions','w') as f:
+			json.dump(predictions,f)
 
-	def Update:
-		pass
+	def getPredictions():
+		with 
+		return predictions
+
+	def update(self):
+		predictions = self.getPredictions()
+		
+		self.dumpToDataBase(predictions)
 
 
 if __name__ == "__main__":
