c07b01150d4804f892ecefd6523f44a262afc333
kuba1 <kubastechly@gmail.com>
2015-03-30 21:54:15 -0700

    predictionStatsUpdater.py

--- predictors/predictionStatsUpdater.py
+++ predictors/predictionStatsUpdater.py
@@ -3,8 +3,18 @@ class PredictionStatsUpdater(object):
 	def PredictionStatsUpdater(self):
 		pass
 
+	def getPredictors(self):
+		predictors = []
+
+		with open('list_of_predictors','r') as f:
+			for line in f:
+				predictors.append(line.rstrip())
+
 	def updatePredictionStats(self):
-		pass
+		predictors = getPredictors(self)
+		
+
+
 
 if __name__ == "__main__":
 	p = PredictionStatsUpdater()
