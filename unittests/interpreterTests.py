ad3163f7bf1781eae0aca8f724b1f68f3b6ba225
kuba1 <kubastechly@gmail.com>
2015-04-03 19:31:33 -0700

    writing unittests

--- unittests/interpreterTests.py
+++ unittests/interpreterTests.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python
- 
+
 import unittest
 
 class InterpreterTests (unittest.TestCase):
-	
+	# unittests for Interpreter
+
+	def setUp(self):
+		""" Setting up for the test """
+		print "InterpreterTests:setUp_:begin"
+		## We'll use this when doing more databasey stuff
+		print "InterpreterTests:setUp_:end"
+
+	def tearDown(self):
+		""" Cleaning up after the test"""
+		print "InterpreterTests:tearDown_:begin"
+		## We'll use this once we actually have databases
+		print "InterpreterTests:tearDown_:end"
+
+	def testCheckBayes(self):
+		a = Interpreter()
+		self.failUnless(a.applyBayes(0.5,0.5,0.5)==0.5)
+		self.failUnless(a.applyBayes(0,1,1)==0)
+		self.failUnless(a.applyBayes(.9,.1,0.5)==4.5)
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
+		
