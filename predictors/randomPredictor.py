3405d2a358d2da651ce138f8309ef742e9b6b32e
kuba1 <kubastechly@gmail.com>
2015-04-17 00:15:18 -0700

    finished basic random predictor

--- predictors/randomPredictor.py
+++ predictors/randomPredictor.py
@@ -1,11 +1,14 @@
 # This predictor is completely random.
 
+from predictor import Predictor
 import random
 
 class RandomPredictor(Predictor):
-	threshold = 0.5
+	def __init__(self):
+		self.threshold = 0.5
+	
 	def goesUp(self):
-		return random.random() > threshold
+		return random.random() > self.threshold
 
 if __name__ == "__main__":
 	print(RandomPredictor().goesUp())
