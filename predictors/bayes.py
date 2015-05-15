88d2b3d834b8143eef6655b46178f302c449dd5c
kuba1 <kubastechly@gmail.com>
2015-05-15 12:22:19 -0700

    changed bayes from class to just a method

--- predictors/bayes.py
+++ predictors/bayes.py
@@ -1,5 +1,4 @@
-class Bayes (object):
-    def apply_bayes(self,prior,pDy,pDn):
-        if pDn == pDy or pDn == 0 or pDy == 0:
-            return prior
-        return pDy/pDn * prior
+def apply_bayes(prior,pDy,pDn):
+    if pDn == pDy or pDn == 0 or pDy == 0:
+        return prior
+    return pDy/pDn * prior
