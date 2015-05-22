696e7567de0a962359495b175a71b62293225bd5
kuba1 <kubastechly@gmail.com>
2015-05-21 17:49:30 -0700

    added comments to bayes_calc desc what fxn does

--- bayes_calc.py
+++ bayes_calc.py
@@ -1,4 +1,5 @@
 def apply_bayes(prior,prediction,pyy,pyn):
+    # Takes in above, spits out posterior according to bayes thm
     pDy = 1-prediction-pyy
     pDn = 1-prediction-pyn
     if pDn == pDy or pDn == 0 or pDy == 0:
